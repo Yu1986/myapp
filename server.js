d56ffae3d698e95e3347eb5b593b2633538407fb
Corey <xiaoxi.xue@adap.tv>
2017-07-16 00:13:05 -0700

    have mocha&chai working together

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 
 // BASE SETUP
 // =============================================================================
+config = require('config');
+
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/myapp');
+mongoose.connect(config.DBHost);
+
+console.log(config.util.getEnv('NODE_ENV'));
+console.log(config.DBHost);
 
 // call the packages we need
 var express    = require('express');        // call express
@@ -17,22 +22,22 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
-var swaggerUi = require('swagger-ui-express'); // line 7 
-var swaggerJSDoc = require('swagger-jsdoc'); // line 8 
+var swaggerUi = require('swagger-ui-express'); 
+var swaggerJSDoc = require('swagger-jsdoc');
 
-var options = { // line 27 
+var options = { 
   swaggerDefinition: {
     info: {
       title: 'Bears', // Title (required) 
       version: '1.0.0', // Version (required) 
     },
   },
-  apis: ['./routes/*.js'], // Path to the API docs 
+  apis: ['./app/routes/*.js'], // Path to the API docs 
 };
 
 var swaggerSpec = swaggerJSDoc(options);
 
-app.get('/api-docs.json', function(req, res) { // line 41 
+app.get('/api-docs.json', function(req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.send(swaggerSpec);
 });
@@ -41,70 +46,34 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec)); // line 45
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
-
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
     console.log('Something is happening.');
     next(); // make sure we go to the next routes and don't stop here
 });
-
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
 // more routes for our API will happen here
-bear = require('./routes/bear')
+bear = require('./app/routes/bear')
 router.route('/bears')
       .get(bear.getBears)
       .post(bear.postBears);
 
 router.route('/bears/:bear_id')
-    .get(function(req, res) {
-    	Bear.findById(req.params.bear_id, function(err, bear) {
-    		if (err)
-    			res.send(err);
-    		res.json(bear);
-    	})
-    })
-    // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
-    .put(function(req, res) {
-        // use our bear model to find the bear we want
-        Bear.findById(req.params.bear_id, function(err, bear) {
-
-            if (err)
-                res.send(err);
-
-            bear.name = req.body.name;  // update the bears info
-
-            // save the bear
-            bear.save(function(err) {
-                if (err)
-                    res.send(err);
-
-                res.json({ message: 'Bear updated!' });
-            });
-
-        });
-    })
-    // delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
-    .delete(function(req, res) {
-        Bear.remove({
-            _id: req.params.bear_id
-        }, function(err, bear) {
-            if (err)
-                res.send(err);
-
-            res.json({ message: 'Successfully deleted' });
-        });
-    });
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+    .get(bear.getBearById)
+    .put(bear.update)
+    .delete(bear.remove);
 
 // START THE SERVER
 // =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port);
+
+module.exports = app; // for testing
